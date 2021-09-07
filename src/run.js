85e9d4634a7ea60072e6ada7cafff840edd5a5e4
plukenis <ieva.zaboole@gmail.com>
2021-09-07 15:16:18 +0300

    update 09.07

--- src/run.js
+++ src/run.js
@@ -3,7 +3,7 @@ import * as path from "path";
 import * as fs from "fs/promises";
 
 const WEB = "web";
-const PORT = 3000;
+const PORT = 8080;
 
 function readHeaders(socket) {
   return new Promise((resolve, reject) => {
@@ -15,7 +15,7 @@ function readHeaders(socket) {
       for (const line of lines) {
         if (line === "") {
           resolve(res);
-          return
+          return;
         }
       }
     });
@@ -29,43 +29,55 @@ function readHeaders(socket) {
 }
 
 async function handler(socket) {
+  let data;
   try {
-    const data = await readHeaders(socket);
-    // console.log(data);
+    data = await readHeaders(socket);
     const lines = data.split("\r\n");
-    // console.log(lines[0]);
-    const [, resource] = lines[0].split(" ");
-    // console.log(resource);
+    let [, resource] = lines[0].split(" ");
     const f = path.join(WEB, resource);
-    // console.log(f);
-  
+
     let res = "";
     try {
-      const response = await fs.readFile(f, {
-        encoding: "utf8"
-      });
-      res += "HTTP/1.1 200 OK\r\n";
-      res += "\r\n";
-      res += response;
+      const stat = await fs.stat(f);
+      if (stat.isFile()) {
+        const response = await fs.readFile(f, {
+          encoding: "utf8",
+        });
+        res += "HTTP/1.1 200 OK\r\n";
+        res += "\r\n";
+        res += response;
+      } else if (stat.isDirectory()) {
+        const files = await fs.readdir(f);
+        resource += (!resource.endsWith("/")) ? "/" : "";
+        res += "HTTP/1.1 200 OK\r\n";
+        res += "\r\n";
+        res += `<html>\r\n`;
+        res += `<body>\r\n`;
+        res += `Direktorija ${resource}<br>\r\n`;
+        if (resource != "/") {
+          res += `<a href="${resource + ".."}">..</a><br>\r\n`;
+        }
+        res += `<ul>\r\n`;
+        for (const fileName of files) {
+          res += `<li><a href="${resource + fileName}">${fileName}</a></li>`;
+        }
+        res += "</ul></body></html>";
+      } else {
+        res += "HTTP/1.1 400 Bad Request\r\n";
+        res += "\r\n";
+      }
     } catch (err) {
       res += "HTTP/1.1 404 Not Found\r\n";
       res += "\r\n";
     }
-    // console.log(res);
-  
-    // let res = "HTTP/1.1 404 Not found\r\n";
-    // res += "<html>\r\n";
-    // res += "<body>\r\n";
-    // res += "<h1>Hello World</h1>\r\n";
-    // res += "</body>\r\n";
-    // res += "</html>\r\n";
     socket.write(res, "utf8");
   } catch (err) {
+    console.log(data);
     console.log("Klaida", err);
     let res = "HTTP/1.1 400 Bad Request\r\n";
     res += "\r\n";
     socket.write(res, "utf8");
-} finally {
+  } finally {
     socket.end();
   }
 }
@@ -74,4 +86,3 @@ const srv = net.createServer(handler);
 
 srv.listen(PORT);
 console.log("Server started");
-
